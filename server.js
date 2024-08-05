9ed3bf550221edc62be1c52f0706504e27a7259f
Arsany199 <arsanykamal30@gmail.com>
2024-08-05 04:05:58 -0700

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import controllerRouting from './routes/index';
 
-const myapp = express();
-const myport = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
-myapp.use(express.json());
+app.use(express.json());
 
-controllerRouting(myapp);
+controllerRouting(app);
 
-myapp.listen(myport, () => {
-  console.log(`Server running on port ${myport}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
-export default myapp;
+export default app;
